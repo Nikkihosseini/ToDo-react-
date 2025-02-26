07184843a377cf3cfacff6a24e3e2be8702e383b
Nikkihosseini <niekal00@gmail.com>
2025-02-26 10:32:17 +0330

    add inputValueHandler function

--- src/ToDo/ToDo.jsx
+++ src/ToDo/ToDo.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react"
 
 
 export default function ToDo(){
+    const [toDoList , setToDoList] = useState([])
+    const [newToDo , setNewToDo] = useState('')
+
+    function inputValueHandler(event){
+        setNewToDo(event.target.value)
+    }
+ 
+
     return(
         <>
-           <div className="flex items-center justify-center mt-10 gap-x-2">
+           <div className="flex flex-col items-center justify-center mt-10 gap-y-2">
               <div className="flex items-center gap-x-2">
-                <input className="rounded-lg border border-zinc-950 py-1.5 px-2" type="text" />
-                <button className="rounded-lg border border-zinc-950 p-1.5 cursor-pointer">Add</button>
+                <input onChange={inputValueHandler} className="rounded-lg border border-zinc-950 py-1.5 px-2" type="text" />
+                <button onClick={addToDo} className="rounded-lg border border-zinc-950 p-1.5 cursor-pointer">Add</button>
               </div>
               <div>
-                <h1></h1>
+                  
               </div>
            </div>
-    
         </>
     )
 }

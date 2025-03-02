e70ed4477cf5d792a7b59441f9886940e85a8e75
Nikkihosseini <niekal00@gmail.com>
2025-03-02 10:10:42 +0330

    responsive code

--- src/ToDo/ToDoApp.jsx
+++ src/ToDo/ToDoApp.jsx
@@ -35,11 +35,11 @@ export default function ToDoApp(){
 
     return(
         <>
-        <div className="relative h-[70vh] w-[22vw]">
+        <div className="relative h-[70vh] w-[80vw] sm:w-[50vw] md:w-[38vw] lg:w-[30vw] 2xl:w-[25vw]">
             <span className="absolute top-[-60px] left-[-60px] block rounded-full size-50 bg-linear-45 from-pink-80 to-orange-60 -z-10"></span>
-            <span className="absolute bottom-[149px] right-[-104px] block rounded-full size-70 bg-linear-45 from-green-60 to-purple-40 -z-10"></span>
+            <span className="absolute bottom-[110px] sm:bottom-[149px] right-[-104px] block rounded-full size-70 bg-linear-45 from-green-60 to-purple-40 -z-10"></span>
             <span className="absolute bottom-[-13px] left-[-13px] block rounded-full size-40 bg-linear-45 from-pink-90 to-pink-40 -z-10"></span>
-            <div className="flex flex-col items-center justify-start relative max-h-[70vh] w-[22vw] z-40 bg-alpha-50 border border-blue-20 backdrop-blur-[20px] rounded-2xl p-4 scroll-y overflow-y-scroll">
+            <div className="flex flex-col items-center justify-start relative max-h-[70vh] w-[80vw] sm:w-[50vw] md:w-[38vw] lg:w-[30vw] 2xl:w-[25vw] z-40 bg-alpha-50 border border-blue-20 backdrop-blur-[20px] rounded-2xl p-4 scroll-y overflow-y-scroll">
                 <form className="relarive flex flex-col text-left gap-x-2 min-h-[8rem] w-full bg-alpha-60 backdrop-blur-[20px] rounded-2xl p-2">
                     <h1 className="font-Archivo-bold text-xl text-sky-950">Create task:</h1>
                     <textarea onChange={inputValueHandler} className="text-start bloke w-[220px] h-[72px] outline-none text-sky-50 pb-1.5 px-2 overflow-y-scroll resize-none" type="text" placeholder="New task..."/>
